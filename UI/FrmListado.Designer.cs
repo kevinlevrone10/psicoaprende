4667f8ace3c1f1c599fd450be9af686299c1db20
kevin Gutierrez <gutierrezvinicius9@gmail.com>
2023-06-13 00:38:01 -0600

    ultimo

--- UI/FrmListado.Designer.cs
+++ UI/FrmListado.Designer.cs
@@ -28,12 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBusqueda = new System.Windows.Forms.TextBox();
+            this.Buscar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.celular = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombres = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Edit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBusqueda
+            // 
+            this.textBusqueda.Location = new System.Drawing.Point(83, 37);
+            this.textBusqueda.Name = "textBusqueda";
+            this.textBusqueda.Size = new System.Drawing.Size(100, 20);
+            this.textBusqueda.TabIndex = 6;
+            // 
+            // Buscar
+            // 
+            this.Buscar.Location = new System.Drawing.Point(2, 34);
+            this.Buscar.Name = "Buscar";
+            this.Buscar.Size = new System.Drawing.Size(75, 23);
+            this.Buscar.TabIndex = 5;
+            this.Buscar.Text = "Buscar";
+            this.Buscar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.nombre,
+            this.apellido,
+            this.Fecha,
+            this.celular,
+            this.Nombres,
+            this.Direccion,
+            this.Edit});
+            this.dataGridView1.Location = new System.Drawing.Point(-3, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(818, 211);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Codigo";
+            this.Codigo.Name = "Codigo";
+            this.Codigo.ReadOnly = true;
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.Name = "nombre";
+            this.nombre.ReadOnly = true;
+            // 
+            // apellido
+            // 
+            this.apellido.HeaderText = "Apellido";
+            this.apellido.Name = "apellido";
+            this.apellido.ReadOnly = true;
+            // 
+            // Fecha
+            // 
+            this.Fecha.HeaderText = "Fecha";
+            this.Fecha.Name = "Fecha";
+            // 
+            // celular
+            // 
+            this.celular.HeaderText = "telefono";
+            this.celular.Name = "celular";
+            this.celular.ReadOnly = true;
+            // 
+            // Nombres
+            // 
+            this.Nombres.HeaderText = "Responsable";
+            this.Nombres.Name = "Nombres";
+            this.Nombres.ReadOnly = true;
+            // 
+            // Direccion
+            // 
+            this.Direccion.HeaderText = "Domicilio";
+            this.Direccion.Name = "Direccion";
+            this.Direccion.ReadOnly = true;
+            // 
+            // Edit
+            // 
+            this.Edit.HeaderText = "Actualizar";
+            this.Edit.Name = "Edit";
+            this.Edit.ReadOnly = true;
+            // 
+            // FrmListado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBusqueda);
+            this.Controls.Add(this.Buscar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FrmListado";
             this.Text = "FrmListado";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBusqueda;
+        private System.Windows.Forms.Button Buscar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn celular;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombres;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Direccion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Edit;
     }
 }
--- UI/FrmListado.cs
+++ UI/FrmListado.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SistemaPsicoaprende.AppDatos;
+using SistemaPsicoaprende.Controlador;
+using SistemaPsicoaprende.Negocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +18,95 @@ namespace SistemaPsicoaprende.UI
         public FrmListado()
         {
             InitializeComponent();
+        }
+
+        public void Listado()
+        {
+
+            foreach (var est in CtrlEstudiante.Buscar())
+            {
+                dataGridView1.Rows.Add(est.cod_Alumno, est.nom_Alumno, est.ape_Alumno, est.fechaNac_Alumno, est.telfResp_Alumno, est.nomResp_Alumno, est.domicilio_Alumno, "Edit");
+            }
+        }
+
+        private void FrmListado_Load(object sender, EventArgs e)
+        {
+            Listado();
+            dataGridView1.CellClick += dataGridView1_CellClick;
+
+
+        }
+
+        private void Buscar_Click(object sender, EventArgs e)
+        {
+            string busqueda = textBusqueda.Text.Trim();
+
+            if (string.IsNullOrEmpty(busqueda))
+            {
+                MessageBox.Show("Por favor, ingrese un valor de búsqueda válido.", "Búsqueda",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            Alumnos estudiante = CtrlEstudiante.buscar(busqueda);
+
+            if (estudiante == null)
+            {
+                MessageBox.Show("No se encontró ningún estudiante con el código especificado.", "Búsqueda",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            MostrarEstudianteEnDataGridView(estudiante);
+        }
+
+        private void MostrarEstudianteEnDataGridView(Alumnos estudiante)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Rows.Add(estudiante.cod_Alumno, estudiante.nom_Alumno, estudiante.ape_Alumno,
+                                   estudiante.fechaNac_Alumno, estudiante.telfResp_Alumno, estudiante.nomResp_Alumno,
+                                   estudiante.domicilio_Alumno, "Edit");
+        }
+
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["Edit"].Index && e.RowIndex >= 0)
+            {
+                // Obtener los valores de la fila seleccionada
+                DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+                string codigo = selectedRow.Cells[0].Value.ToString();
+
+                // Obtener el estudiante completo utilizando el código
+                Alumnos estudiante = CtrlEstudiante.buscar(codigo);
+
+                if (estudiante != null)
+                {
+                    // Obtener los valores de los campos del estudiante
+                    string nombre = estudiante.nom_Alumno;
+                    string apellido = estudiante.ape_Alumno;
+                    DateTime fechaNacimiento = estudiante.fechaNac_Alumno;
+                    string telefono = estudiante.telfResp_Alumno;
+                    string colegio = estudiante.colegio_Alumno;
+                    string responsable = estudiante.nomResp_Alumno;
+                    string domicilio = estudiante.domicilio_Alumno;
+                    string grado = estudiante.gradoAcad_Alumno;
+                    string evaluacion = estudiante.evaluacion_Alumno;
+                    int municipioId = estudiante.MunicipioId;
+                    int departamentoId = estudiante.DepartamentoId;
+
+                    // Abrir el formulario de FrmEstudiante y pasar los datos como argumentos
+                    FrmEstudiante frmEstudiante = new FrmEstudiante(codigo, nombre, apellido, fechaNacimiento, telefono, colegio, responsable, domicilio, grado, evaluacion, municipioId, departamentoId);
+                    frmEstudiante.ShowDialog();
+                }
+            }
+        }
+
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
