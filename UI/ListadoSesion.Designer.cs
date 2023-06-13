8b07ffb599bf810d54631e36aa40bdded83e5ea2
kevin Gutierrez <gutierrezvinicius9@gmail.com>
2023-06-13 13:06:20 -0600

    confirmado doble

--- UI/ListadoSesion.Designer.cs
+++ UI/ListadoSesion.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBusqueda = new System.Windows.Forms.TextBox();
+            this.Buscar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Edit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBusqueda
+            // 
+            this.textBusqueda.Location = new System.Drawing.Point(197, 115);
+            this.textBusqueda.Name = "textBusqueda";
+            this.textBusqueda.Size = new System.Drawing.Size(100, 20);
+            this.textBusqueda.TabIndex = 12;
+            // 
+            // Buscar
+            // 
+            this.Buscar.Location = new System.Drawing.Point(312, 115);
+            this.Buscar.Name = "Buscar";
+            this.Buscar.Size = new System.Drawing.Size(75, 23);
+            this.Buscar.TabIndex = 11;
+            this.Buscar.Text = "Buscar";
+            this.Buscar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.codigo,
+            this.fecha,
+            this.cantidad,
+            this.Edit});
+            this.dataGridView1.Location = new System.Drawing.Point(180, 151);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(440, 185);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // codigo
+            // 
+            this.codigo.HeaderText = "Codigo";
+            this.codigo.Name = "codigo";
+            this.codigo.ReadOnly = true;
+            // 
+            // fecha
+            // 
+            this.fecha.HeaderText = "Fecha";
+            this.fecha.Name = "fecha";
+            this.fecha.ReadOnly = true;
+            // 
+            // cantidad
+            // 
+            this.cantidad.HeaderText = "Cantidad";
+            this.cantidad.Name = "cantidad";
+            this.cantidad.ReadOnly = true;
+            // 
+            // Edit
+            // 
+            this.Edit.HeaderText = "Actualizacion";
+            this.Edit.Name = "Edit";
+            this.Edit.ReadOnly = true;
+            // 
+            // ListadoSesion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBusqueda);
+            this.Controls.Add(this.Buscar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ListadoSesion";
             this.Text = "ListadoSesion";
+            this.Load += new System.EventHandler(this.ListadoSesion_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBusqueda;
+        private System.Windows.Forms.Button Buscar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Edit;
     }
 }
--- UI/ListadoSesion.cs
+++ UI/ListadoSesion.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SistemaPsicoaprende.AppDatos;
+using SistemaPsicoaprende.Controlador;
+using SistemaPsicoaprende.Negocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +18,86 @@ namespace SistemaPsicoaprende.UI
         public ListadoSesion()
         {
             InitializeComponent();
+        }
+
+        private void Buscar_Click(object sender, EventArgs e)
+        {
+            string busqueda = textBusqueda.Text.Trim();
+            if (string.IsNullOrEmpty(busqueda))
+            {
+                MessageBox.Show("Por favor, ingrese un valor de búsqueda válido.", "Búsqueda",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            Sesiones sesiones = CtrlSesion.buscar(busqueda);
+
+            if (sesiones == null)
+            {
+                MessageBox.Show("No se encontró ningún estudiante con el código especificado.", "Búsqueda",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            MostrarEstudianteEnDataGridView(sesiones);
+        }
+
+
+        private void MostrarEstudianteEnDataGridView(Sesiones sesiones)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Rows.Add(sesiones.cod_Sesion, sesiones.fecha_Sesion, sesiones.cantHoras_Sesion, "Edit");
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["Edit"].Index && e.RowIndex >= 0)
+            {
+                // Obtener los valores de la fila seleccionada
+                DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+                string codigo = selectedRow.Cells[0].Value.ToString();
+
+                // Obtener la sesion completa utilizando el código
+                Sesiones sesion = CtrlSesion.buscar(codigo);
+
+                if (sesion != null)
+                {
+                    // Obtener los valores de los campos de la sesion
+                    DateTime fecha = sesion.fecha_Sesion;
+                    int cantidad = sesion.cantHoras_Sesion;
+                    int facturaId = sesion.FacturaId;
+                    int trabajadorId = sesion.TrabajadorId;
+
+                    // Abrir el formulario de FrmEstudiante y pasar los datos como argumentos
+                    FrmSesiones frmses = new FrmSesiones(codigo, fecha, cantidad, trabajadorId, facturaId);
+                    frmses.ShowDialog();
+                }
+            }
+        }
+
+        public void Listado()
+        {
+
+            foreach (var ses in CtrlSesion.Buscar())
+            {
+                dataGridView1.Rows.Add(ses.cod_Sesion, ses.fecha_Sesion, ses.cantHoras_Sesion, "Edit");
+            }
+        }
+
+        private void ListadoSesion_Load(object sender, EventArgs e)
+        {
+            Listado();
+            dataGridView1.CellClick += dataGridView1_CellClick;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBusqueda_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
