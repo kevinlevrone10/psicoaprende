b49b2b69c8bcf379ddd2ef3a83c1d854665eb1da
David R <roblespabon@gmail.com>
2023-06-12 20:52:29 -0600

    Login realizado

--- Controlador/CtrlLogin.cs
+++ Controlador/CtrlLogin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaPsicoaprende.AppDatos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,17 @@ using System.Threading.Tasks;
 
 namespace SistemaPsicoaprende.Controlador
 {
-    internal class CtrlLogin
+    public class CtrlLogin
     {
-    }
+
+        public static Usuario BuscarUsuario(string nomUsuario)
+        {
+            // Instanciamos la clase AppDatoContext de la capa de datos
+            SistemaPsicoaprendeConnection contexto = new SistemaPsicoaprendeConnection();
+            // Realizamos la consulta para obtener el usuario por nombre de usuario
+            Usuario usuario = contexto.Usuario.FirstOrDefault(u => u.NomUsuario == nomUsuario);
+            //Retorna el nombre del usuario
+            return usuario;
+        }//Fin del método de BuscarUsuario
+    }//Fin de la clase
 }
--- Negocio/Login.cs
+++ Negocio/Login.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SistemaPsicoaprende.AppDatos;
+using SistemaPsicoaprende.Controlador;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,30 @@ using System.Threading.Tasks;
 
 namespace SistemaPsicoaprende.Negocio
 {
-    internal class Login
+    public class Login
     {
-    }
+
+        public bool ValidarCredenciales(string nombre, string contraseña)
+        {
+            //CtrlLogin login = new CtrlLogin();
+
+            // Llamamos al método ObtenerUsuario por nombre de usuario
+            Usuario usr = CtrlLogin.BuscarUsuario(nombre);
+
+            if (usr != null)
+            {
+                // Si se encontró un usuario con el nombre de usuario proporcionado, comparamos las contraseñas cifradas
+                //string passwordCifrada = Encrypt(contraseña); // Ciframos la contraseña ingresada
+
+                if (usr.PassWrd == contraseña)
+                {
+                    // Las contraseñas coinciden, la validación es exitosa
+                    return true;
+                }
+            }
+            // Las credenciales son inválidas
+            return false;
+        }//Fin del método de ValidarCredenciales
+
+    }//Fin de la clase
 }
